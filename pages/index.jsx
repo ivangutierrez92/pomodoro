bde0839df01d51575cb0c69b5cd9e80a543d070a
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-03-22 15:48:52 -0300

    added export script

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import Footer from '../components/Footer';
 import Pomodoro from '../containers/Pomodoro';
 import styles from '../styles/Home.module.css';
